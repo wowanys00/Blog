e676515d5455fb03087635a3b0ee8fb5df538851
wowanys00 <wowanys00@gmail.com>
2018-12-18 17:22:24 +0200

    add menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'persons/profile'
+ 
   devise_for :users
-	root 'posts#index'
+  root 'posts#index'
   resources :posts
-  get 'persons/profile', as: 'user_root'
+  resources :persons
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
